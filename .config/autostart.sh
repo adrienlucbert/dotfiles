21f95eaa66f4ba90715cec0eeead7d32d9f3cc20
adrienlucbert <adrien.lucbert@epitech.eu>
2020-12-13 17:33:13 +0100

    fix autostart and desktops distribution
    pgrep with bash did not exit with an error when no process is found,
    causing multiple instances of 'once' rules to be run
    desktops distribution now uses xrandr to list active monitors, instead
    of polybar

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ### Helpers {
 
 action="$1";
@@ -5,38 +7,41 @@ action="$1";
 # Is logout command (Cleanup)
 is_kill() { [[ "$action" == "kill" ]]; }
 
+# Is process $1 running
+is_running() { test $(pgrep $1); }
+
 # Only run when there are no windows on the screen
 on_startup() { is_kill || [[ "$(wmctrl -l | wc -l)" = "0" ]] && $@ & }
 
 # Run only once. If an instance is already running, noop
 once() {
-  local name=$1; shift;
+  local name=$1; shift
   if (is_kill); then
-    killall "$name";
+    killall $name
   else
-    pgrep $name || $@ &
-  fi;
+    is_running $name || $@ &
+  fi
 }
 
 # Kill previous instance and run again
 run() {
-  local name=$1; shift;
-  [[ ! -z "$name" ]] && killall -9 $name && sleep 0.05 && echo "killed $name";
+  local name=$1; shift
+  [[ ! -z "$name" ]] && is_running $name && killall -9 $name
   is_kill || $@ &
 }
 # }
 
 
 ##### Autostart {{{
-  echo "[Autostart]: Running daemons";
 
-  run "polybar" $HOME/.config/polybar/launch.sh
+  # Status bar
+  run "polybar" ~/.config/polybar/launch.sh
 
   # Better key autorepeat rate
   run "" xset r rate 200 50
 
   # Activate numlock
-  run "numlockx" numlockx;
+  run "numlockx" numlockx
 
   # Wallpaper
   run "" feh --bg-fill ~/.wall
@@ -45,19 +50,20 @@ run() {
   run "idlelock" idlelock
 
   # Notification daemon
-  once "dunst" dunst -config ~/.config/dunst/dunstrc;
+  once "dunst" dunst -config ~/.config/dunst/dunstrc
 
   # Compositor
   # run "picom" picom --experimental-backends --config ~/.config/compton.conf;
 
   # Network manager applet
-  once "nm-applet" nm-applet;
+  once "nm-applet" nm-applet
 
   # Automatic screen temperature and brightness
   once "redshift" redshift -c ~/.config/redshift/redshift.conf
 
   # Hotkey daemon 
   run "sxhkd" sxhkd -c ~/.config/sxhkd/sxhkdrc -m 1
+
 # }}}
 
 disown;
